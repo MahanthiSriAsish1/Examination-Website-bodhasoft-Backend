0a0c7d7d7c706f4e083ea86412d6b0c4fa24733e
MahanthiSriAsish1 <110473671+MahanthiSriAsish1@users.noreply.github.com>
2024-02-03 13:14:47 +0530

    -third commit

---
+++ db_connect/db_connect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const dbConnect = (dbName) => {
+
+    const connect_uri = `mongodb+srv://Asish:Asish2002@cluster0.it1lvdq.mongodb.net/${dbName}`;
+
+    mongoose.connect(connect_uri);
+    const db = mongoose.connection;
+    db.on('connected', () => {
+        console.log(`Connected to MongoDB database: ${dbName}`);
+    });
+    db.on('error', (err) => {
+        console.error(`MongoDB connection error: ${err}`);
+    });
+};
+
+module.exports = dbConnect;
